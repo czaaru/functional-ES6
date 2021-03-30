66c6db6a8e85ae547fbda326bba109aef2a3fbfc
Cezary Kuźma <czeny98@gmail.com>
2021-03-30 19:04:50 +0200

    feat: add finished challenge

---
+++ ch1.js
@@ -0,0 +1,24 @@
+//function below works but reduce is used in a wrong way
+
+// const map = (arr, func) => {
+//   const mapArray = [];
+//   const addValue = (value) => mapArray.push(value);
+
+//   arr.reduce((_, x) => {
+//     addValue(func(x));
+//   }, 0);
+//   return mapArray;
+// };
+
+const map = (arr, func) => arr.reduce((acc, x) => [...acc, func(x)], []);
+
+// testing if it works:
+
+// Should be [2, 4, 6]
+console.log(map([1, 2, 3], (x) => x * 2));
+
+// Should be [-5, -6, -7, -8, -9, -10]
+console.log(map([5, 6, 7, 8, 9, 10], (x) => -x));
+
+// Should be ['A', 'B', 'C', 'D']
+console.log(map(["a", "b", "c", "d"], (x) => x.toUpperCase()));
